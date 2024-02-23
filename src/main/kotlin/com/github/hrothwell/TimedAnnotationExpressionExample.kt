c2569613583ae2243ebf04af0a1a2539656aa276
Haiden <46227616+hrothwell@users.noreply.github.com>
2024-02-23 14:19:31 -0700

    working test example

--- src/main/kotlin/com/github/hrothwell/TimedAnnotationExpressionExample.kt
+++ src/main/kotlin/com/github/hrothwell/TimedAnnotationExpressionExample.kt
@@ -12,7 +12,18 @@ open class TimedAnnotationExpressionExample {
 
 
     @Timed(value = "#{this.className}", extraTags = ["method", "#{this.className}.doSomething"]) // treated as evaluated annotation metadata
-//    @Timed(value = "my_metric", extraTags = ["method", "#{this.className}.doSomething"]) // not treated as evaluated annotation metadata
+//    @Timed(value = "TimedAnnotationExpressionExample", extraTags = ["method", "#{this.className}.doSomething"]) // not treated as evaluated annotation metadata
+    open fun doSomething() {
+        for(i in 1..500){
+            i * i
+        }
+    }
+}
+
+// Strictly for proving in tests that metrics can be found/have proper tags
+@Singleton
+open class TimedThatDoesWork {
+    @Timed(value = "working_metric", extraTags = ["method", "doSomething"])
     open fun doSomething() {
         for(i in 1..500){
             i * i
--- src/test/groovy/com/github/hrothwell/TimedAnnotationExpressionExampleSpec.groovy
+++ src/test/groovy/com/github/hrothwell/TimedAnnotationExpressionExampleSpec.groovy
@@ -1,10 +1,12 @@
 package com.github.hrothwell
 
 import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Tag
 import io.micronaut.runtime.EmbeddedApplication
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import jakarta.inject.Inject
 import spock.lang.Specification
+import spock.lang.Unroll
 
 @MicronautTest
 class TimedAnnotationExpressionExampleSpec extends Specification{
@@ -12,17 +14,23 @@ class TimedAnnotationExpressionExampleSpec extends Specification{
     @Inject
     EmbeddedApplication<?> app
 
+    @Unroll
     def 'calling method should make metric available with right tags'(){
         given:
         def context = app.getApplicationContext()
         def meterRegistry = context.getBean(MeterRegistry)
-        def callMe = context.getBean(TimedAnnotationExpressionExample)
+        def bean = context.getBean(type)
 
         when:
-        callMe.doSomething()
+        bean.doSomething()
 
         then:
-        def meter = meterRegistry.getMeters().find{it.id.name == "TimedAnnotationExpressionExample" }
-        meter.id.tags.contains("TimedAnnotationExpressionExample.doSomething")
+        def meter = meterRegistry.getMeters().find{ it.id.name == name }
+        meter.id.tags.contains(tag)
+
+        where:
+        type                             | name                               | tag
+        TimedAnnotationExpressionExample | "TimedAnnotationExpressionExample" | Tag.of("method", "TimedAnnotationExpressionExample.doSomething") // fails
+        TimedThatDoesWork                | "working_metric"                   | Tag.of("method", "doSomething") // passes
     }
 }
